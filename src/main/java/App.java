91168fdc6f38f38ba55ade0971c8781822c0716b
Robbie Mcilwaine <robbiemcilwaine99@gmail.com>
2024-06-06 12:22:20 +0100

    add to line 6 main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD!!!");
         System.out.println("ADDED THIS TO LINE 4");
+
+        System.out.println("ADDED THIS TO LINE 6 ON main");
     }
 }

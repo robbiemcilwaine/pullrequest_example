f2fef98aa93666589c0b402c51b9cb93c8767164
Robbie Mcilwaine <robbiemcilwaine99@gmail.com>
2024-06-06 12:16:30 +0100

    change line 4

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD!!!");
-        System.out.println("ADDED THIS ON LINE 4");
+        System.out.println("ADDED THIS TO LINE 4");
     }
 }
